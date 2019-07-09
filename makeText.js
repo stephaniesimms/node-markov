05b52877445b1d5257807f7dca785a40fab0d5a5
Stephanie Simms <stephanie.simms@gmail.com>
2019-07-09 11:47:24 -0700

    completed markov machine parts 1-2, added bigrams, caps words to algorithm

--- makeText.js
+++ makeText.js
@@ -1,2 +1,39 @@
+const fs = require('fs');
+const axios = require('axios');
+const { MarkovMachine } = require('./markov');
+
 /** Command-line tool to generate Markov text. */
 
+function readTextFile(path) {
+  fs.readFile(path, 'utf8', function(err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      let mm = new MarkovMachine(data);
+      console.log(mm.makeText());
+    }
+  });
+}
+
+async function readUrl(path) {
+  try {
+    let response = await axios.get(path);
+    let mm = new MarkovMachine(response.data);
+    console.log(mm.makeText());
+  } catch (err) {
+    console.error(`Error fetching ${path}: ${err}`);
+    process.exit(1);
+  }
+}
+
+let readMethod = process.argv[2];
+let path = process.argv[3];
+
+if (readMethod === 'file') {
+  readTextFile(path);
+} else if (readMethod === 'url') {
+  readUrl(path);
+} else {
+  console.error(`Error, invalid read method. ${readMethod} should be 'file' or 'url'.`);
+}
--- markov.js
+++ markov.js
@@ -17,13 +17,55 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    this.chains = {};
+    for (let i = 1; i < this.words.length; i++) {
+      let wordKey = this.words[i-1] + " " + this.words[i];
+      let nextWord = this.words[i+1];
+      if (nextWord === undefined) {
+        nextWord = null;
+      }
+      if (this.chains[wordKey]) { 
+        this.chains[wordKey].push(nextWord);
+      } else {
+        this.chains[wordKey] = [nextWord];
+      }
+    }
   }
-
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let isCapital = false;
+    let firstBigram;
+    let keys = Object.keys(this.chains);
+
+    while (!isCapital) {
+      firstBigram = getRandomWordFromArray(keys);
+      isCapital = firstBigram[0] === firstBigram[0].toUpperCase();
+    }
+
+    firstBigram = firstBigram.split(" ");
+    let previousWord = firstBigram[0];
+    let currWord = firstBigram[1];
+    let textArr = [previousWord];
+    let wordCount = 1;
+  
+    while (wordCount < numWords || currWord === null) {
+      let bigram = previousWord + " " + currWord
+      textArr.push(currWord);
+      previousWord = currWord;
+      currWord = getRandomWordFromArray(this.chains[bigram]);
+      wordCount++;
+    }
+    return textArr.join(" ");
   }
 }
+
+function getRandomWordFromArray(arr) {
+  let i = Math.floor(Math.random() * arr.length);
+  return arr[i];
+}
+
+module.exports = {
+  MarkovMachine: MarkovMachine
+};
